5b528373a2ab28aac8ed661f60a88d107246759c
Bonhin <eduardo.bonhin@gmail.com>
2022-06-16 17:42:36 -0300

    Atualização Cartão Credito

--- ProjetoFinalPOOI/CartaoCredito.cs
+++ ProjetoFinalPOOI/CartaoCredito.cs
@@ -8,23 +8,38 @@
 
         public string? NomeCartao { get; set; }
 
-    public CartaoCredito(long pNºCartaoCredito, string pDataValidade, string pNomeCartao)
+    public CartaoCredito(long pNumero, string pDataValidade, string pNomeCartao)
         {
-            Numero = pNºCartaoCredito;
+            Numero = pNumero;
             DataValidade = pDataValidade; ;
-            this.NomeCartao = pNomeCartao;
+            NomeCartao = pNomeCartao;
+        }
+    
+    public static CartaoCredito Ler()
+        {
+            Console.WriteLine("Digite o numero do cartão");
+            long pNumero = long.Parse(Console.ReadLine().Trim());
+
+            Console.WriteLine("Digite a data de validade do cartão");
+            string? pDataValidade = Console.ReadLine().Trim();
+
+            Console.WriteLine("Digite o nome escrito no cartão");
+            string? pNomeCartao = Console.ReadLine().Trim();
+
+            return new CartaoCredito(pNumero, pDataValidade, pNomeCartao);
         }
 
-    public void AlterarCadastrao()
-        {
-            Console.WriteLine("Insira o novo número do cartão");
-            Numero = long.Parse(Console.ReadLine());
 
-            Console.WriteLine("Insira a data de validade do cartão");
-            DataValidade = Console.ReadLine();
+    public void AlterarCadastro()
+        {
+            Console.WriteLine("Digite o número do cartão");
+            Numero = long.Parse(Console.ReadLine().Trim());
+
+            Console.WriteLine("Digite a data de validade do cartão");
+            DataValidade = Console.ReadLine().Trim();
           
-            Console.WriteLine("Insira o nome escrito no cartão");
-            NomeCartao = Console.ReadLine();
+            Console.WriteLine("Digite o nome escrito no cartão");
+            NomeCartao = Console.ReadLine().Trim();
         }
 
     }
