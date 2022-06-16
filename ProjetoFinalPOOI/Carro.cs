72cce7ef87ac231b300a3ca09c603eb9d7bd8650
André Felipe Sampaio Boschetti <andresampaio@id.uff.br>
2022-06-16 18:18:08 -0300

    Modificação na classe Carro

--- ProjetoFinalPOOI/Carro.cs
+++ ProjetoFinalPOOI/Carro.cs
@@ -1,12 +1,64 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ProjetoFinalPOOI
+﻿namespace ProjetoFinalPOOI
 {
-    internal class Carro
+    public class Carro
     {
+        public string Placa;
+        public string Categoria;
+        public int ValorCategoria;
+        public const int a = 60;
+        public const int b = 70;
+        public const int c = 80;
+        public int Ano;
+        public long Km;
+        public string Modelo;
+        public bool alugado = false;
+
+
+        public Carro(string placa,string categoria, int ano, long km, string modelo, bool alugado)
+
+        {
+            Placa = placa;
+            Categoria = AtribuiValorCategoria(categoria);
+            //ValorCategoria = valorCategoria;
+            Ano = ano;
+            Km = km;
+            Modelo = modelo;
+            alugado = alugado;
+
+        }
+
+        public string AtribuiValorCategoria(string categoria)
+        {
+            if (categoria == "a")
+            {
+                ValorCategoria =a;
+            }
+            else if (categoria == "b")
+            {
+                ValorCategoria = b;
+            }
+            else if (categoria == "c")
+            {
+                ValorCategoria =c;
+            }
+            else
+            {
+                ValorCategoria = 0;//Para destacar erro
+            }
+            return categoria;
+        }
+        public void DadosV()
+        {
+            Console.WriteLine("Dados do Carro:");
+            Console.WriteLine("Placa: "+Placa);
+            Console.WriteLine("Categoria: "+Categoria);
+            Console.WriteLine("Valor da Categoria" + ValorCategoria);
+            Console.WriteLine("Ano: "+ Ano);
+            Console.WriteLine("Kilometragem: "+Km);
+            Console.WriteLine("Modelo: "+Modelo);
+            Console.WriteLine("Alugado: "+ (alugado ? "Sim" : "Não"));
+
+        }
     }
+
 }
