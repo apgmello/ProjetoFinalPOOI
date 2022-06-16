1cf3f89d470b890f448a634aa927d36e9f9a9608
Bonhin <eduardo.bonhin@gmail.com>
2022-06-16 17:03:32 -0300

    Atualização Classe Cartão Credito

--- ProjetoFinalPOOI/CartaoCredito.cs
+++ ProjetoFinalPOOI/CartaoCredito.cs
@@ -4,11 +4,17 @@
     {
         public long nºCartaoCredito { get; set; }     
 
-        public string DataValidade { get; set; }  
+        public string? DataValidade { get; set; }  
 
 
         public string? NomeCartao { get; set; }
 
+    public CartaoCredito(long pNºCartaoCredito, string pDataValidade, string pNomeCartao)
+        {
+            this.nºCartaoCredito = pNºCartaoCredito;
+            this.DataValidade = pDataValidade; ;
+            this.NomeCartao = pNomeCartao;
+        }
     public void AlterarCadastrao()
         {
             Console.WriteLine("Insira o novo número do cartão");
