519ac4f870644c2c49d62207a4c9d72e962e0750
Bonhin <eduardo.bonhin@gmail.com>
2022-06-16 16:56:16 -0300

    Inserindo classe cartao credito

---
+++ ProjetoFinalPOOI/CartaoCredito.cs
@@ -0,0 +1,25 @@
+﻿namespace ProjetoFinalPOOI
+{
+    public class CartaoCredito
+    {
+        public long nºCartaoCredito { get; set; }     
+
+        public DateOnly DataValidade { get; set; }  
+
+
+        public string? NomeCartao { get; set; }
+
+    public void AlterarCadastrao()
+        {
+            Console.WriteLine("Insira o novo número do cartão");
+            nºCartaoCredito = long.Parse(Console.ReadLine());
+
+            Console.WriteLine("Insira a data de validade do cartão");
+          
+
+            Console.WriteLine("Insira o nome escrito no cartão");
+            NomeCartao = Console.ReadLine();
+        }
+
+    }
+}
