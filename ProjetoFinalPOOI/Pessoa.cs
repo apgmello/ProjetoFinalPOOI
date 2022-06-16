6426bacf2712a9faf0ea900dd4f0b3f8c70833fb
Ana Mello <depaula.ana@gmail.com>
2022-06-16 15:55:53 -0300

    Add project files.

---
+++ ProjetoFinalPOOI/Pessoa.cs
@@ -0,0 +1,25 @@
+﻿namespace ProjetoFinalPOOI
+{
+    internal class Pessoa
+    {
+        public string Nome { get; }
+        DateOnly DataNascimento { get; }
+        public long Cpf { get; }
+        public long Rg { get; }
+        public string Endereco { get; }
+        public long Telefone { get; }
+        public long Cnh { get; }
+        public string Categoria { get; }
+
+        public Pessoa(string nome, DateOnly dataNascimento, long cpf, long rg, string endereco, long telefone, long cnh, string categoria)
+        {
+            Nome = nome;
+            Cpf = cpf;
+            Rg = rg;
+            Endereco = endereco;
+            Telefone = telefone;
+            Cnh = cnh;
+            Categoria = categoria;
+        }
+    }
+}
---
+++ ProjetoFinalPOOI/Program.cs
@@ -0,0 +1,55 @@
+﻿int opcao;
+
+//uns objetos aqui
+
+//do pra prender o meliante aqui
+Console.Write("Digite uma opção\n" +
+              "(1) Cadastrar pessoa\n" +
+              "(2) Cadastrar Carro\n" +
+              "(3) Alugar ou devolver carro\n" +
+              "(4) Sair\n" +
+              "Opção: ");
+int.TryParse(Console.ReadLine(), out opcao);
+
+switch (opcao)
+{
+    case 1:
+        //tentar retornar ao anterior
+        //usar o mesmo int opção
+        int opcao2;
+        Console.Write("Digite uma opção\n" +
+                      "(1) Cadastrar Funcionário\n" +
+                      "(2) Cadastrar Cliente\n" +
+                      "Opção: ");
+        int.TryParse(Console.ReadLine(), out opcao2);
+        //chamar a classe pessoa - incializar essa "vazia"
+        //baseado na opção do usuario vai instanciar em funcionario ou cliente
+        break;
+    case 2:
+        //pedir os dados metodo Dados()
+        break;
+    case 3:
+        int opcao3;
+        Console.Write("Digite uma opção\n" +
+              "(1) Alugar\n" +
+              "(2) Devolver\n" +
+              "Opção: ");
+        int.TryParse(Console.ReadLine(), out opcao3);
+
+        // if (1)
+        //diponiveis()
+        //alugar()
+
+        //else 
+        //metodos devolução
+
+        break;
+    case 4:
+        return;
+        break;
+
+    default:
+        break;
+}
+
+
