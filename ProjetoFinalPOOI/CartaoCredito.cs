44e5e84f7cb0864abd75641f8a5cdedf255dc0c9
Bonhin <eduardo.bonhin@gmail.com>
2022-06-16 17:18:25 -0300

    Atualização Cartão Credito

--- ProjetoFinalPOOI/CartaoCredito.cs
+++ ProjetoFinalPOOI/CartaoCredito.cs
@@ -2,28 +2,27 @@
 {
     public class CartaoCredito
     {
-        public long nºCartaoCredito { get; set; }     
+        public long Numero { get; set; }     
 
         public string? DataValidade { get; set; }  
-
 
         public string? NomeCartao { get; set; }
 
     public CartaoCredito(long pNºCartaoCredito, string pDataValidade, string pNomeCartao)
         {
-            this.nºCartaoCredito = pNºCartaoCredito;
-            this.DataValidade = pDataValidade; ;
+            Numero = pNºCartaoCredito;
+            DataValidade = pDataValidade; ;
             this.NomeCartao = pNomeCartao;
         }
+
     public void AlterarCadastrao()
         {
             Console.WriteLine("Insira o novo número do cartão");
-            nºCartaoCredito = long.Parse(Console.ReadLine());
+            Numero = long.Parse(Console.ReadLine());
 
             Console.WriteLine("Insira a data de validade do cartão");
             DataValidade = Console.ReadLine();
           
-
             Console.WriteLine("Insira o nome escrito no cartão");
             NomeCartao = Console.ReadLine();
         }
