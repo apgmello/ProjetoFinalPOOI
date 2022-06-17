e4114b0d3c0152a379d1a54829568aa4cadc3d80
Bonhin <eduardo.bonhin@gmail.com>
2022-06-17 16:13:41 -0300

    Criar classe funcionario

---
+++ ProjetoFinalPOOI/Funcionario.cs
@@ -0,0 +1,14 @@
+﻿namespace ProjetoFinalPOOI
+{
+    internal class Funcionario : Pessoa
+    {
+        public long CarteiraTrabalho { get; }
+        public string Cargo { get; }
+
+        public Funcionario(string nome, DateOnly dataNascimento, long cpf, string endereco, string telefone, long cnh, long carteiraTrabalho, string cargo) : base(nome, dataNascimento, cpf, endereco, telefone, cnh)
+        {
+            CarteiraTrabalho = carteiraTrabalho;
+            Cargo = cargo;
+        }
+    }
+}
